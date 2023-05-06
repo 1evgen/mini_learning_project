5409e1d791bbd9f2318db80a21ba85a995f7016a
Evgenii <evgusa83@gmail.com>
2023-05-06 15:42:55 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {UserData} from "./lessen_01/UserData";
+
+
+const dateUser = [
+    {
+        id: 1,
+        name: 'Evgenii',
+        lastName: 'Goncharov',
+        age: 27,
+        gender: "male"
+    } ,
+    {
+        id: 2,
+        name: 'Olga',
+        lastName: 'Norman',
+        age: 18,
+        gender: "female"
+    } ,
+    {
+        id: 3,
+        name: 'Andrey',
+        lastName: 'Ivanov',
+        age: 22,
+        gender: "male"
+    } ,
+    {
+        id: 4,
+        name: 'Kseniya',
+        lastName: 'Proskudina',
+        age: 18,
+        gender: "female"
+
+    } ,
+    {
+        id: 5,
+        name: 'Ilia',
+        lastName: 'Gordon',
+        age: 55,
+        gender: "male"
+    } ,
+    {
+        id: 6,
+        name: 'Kristina',
+        lastName: 'Golikova',
+        age: 58,
+        gender: "female"
+    } ,
+    {
+        id: 7,
+        name: 'Galina',
+        lastName: 'Korovkina',
+        age: 32,
+        gender: "female"
+    } ,
+    {
+        id: 8,
+        name: 'Tania',
+        lastName: 'Artemova',
+        age: 19,
+        gender: "female"
+    } ,
+    {
+        id: 9,
+        name: 'Li',
+        lastName: 'su',
+        age: 15,
+        gender: "male"
+    } ,
+
+]
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserData nameTable={"The date about Users"}  dataUsers={dateUser}/>
     </div>
   );
 }
---
+++ src/lessen_01/UserData.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import s from './UserData.module.css'
+
+type typeMainDate = {
+    id: number,
+    name: string,
+    lastName: string,
+    age: number
+    gender: string
+}
+
+type UserDataType = {
+    dataUsers: typeMainDate[]
+    nameTable: string
+}
+
+export const UserData: React.FC<UserDataType> = (props) => {
+    return (
+        <div className={s.content}>
+            <div className={s.wrapper}>
+            <div>
+                <h2>{props.nameTable}</h2>
+            </div>
+
+            <table className={s.table}>
+                <thead className={s.thead}>
+                <tr >
+                    <th>ID users</th>
+                    <th>Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                    <th>Checked block users</th>
+                    <th>block</th>
+                    <th>unblock</th>
+                    <th>DELETE USERS</th>
+                </tr>
+                </thead>
+                <tbody className={s.tbody}>
+                {props.dataUsers.map((element) => {
+                    return (
+                        <tr  key={element.id}>
+                            <td>{element.id} </td>
+                            <td>{element.name}</td>
+                            <td>{element.lastName}</td>
+                            <td>{element.age}</td>
+                            <td>{element.gender}</td>
+                            <td><button>check is block users</button></td>
+                            <td><button className={s.block}>block users</button></td>
+                            <td><button className={s.unblock}>unblock users</button></td>
+                            <td><button className={s.delete}>DELETE</button></td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+                <div className={s.filters}>
+                <div className={s.gender}>
+                  <span><button>All</button></span>
+                    <span><button>Man</button></span>
+                    <span><button>Woman</button></span>
+                </div>
+                <div className={s.age}>
+                    <span><button>Yong</button></span>
+                    <span><button>Old</button></span>
+                </div>
+                </div>
+        </div>
+
+        </div>
+    )
+}
